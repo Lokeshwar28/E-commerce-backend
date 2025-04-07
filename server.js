ebc3cfaaf3fa2911222a75718bce913e139ce0f0
Lokeshwar28 <reddylokesh142@gmail.com>
2025-04-07 18:03:14 -0500

    Update CORS to allow frontend from Vercel

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ dotenv.config();
 console.log("ENV Loaded:", process.env.POSTGRES_URL);
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://e-commerce-frontend-three-psi.vercel.app",
+  "https://71eb-18-118-2-28.ngrok-free.app",
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -22,6 +31,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 
 app.use("/api/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+  res.send("E-commerce backend is running ✅");
+});
 
 const PORT = process.env.PORT || 5001;
 connectDB().then(() =>
